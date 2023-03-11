2647adb47f3c1873b334a2fa27a0852377f62074
Nick Engelhardt <enickgoku2@gmail.com>
2023-03-11 10:41:29 -0500

    progress commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import About from './components/About'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   )
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='container'>
+        {/* add image here */}
+        <div className='col-2'>
+          <h1>About Us</h1>
+          <span className='line'></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
